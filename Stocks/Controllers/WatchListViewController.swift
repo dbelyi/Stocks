bc2a2941307562f9aa1132d497e6e0b00091eff6
DanilaWhite <60770374+smthinthewayy@users.noreply.github.com>
2023-05-11 13:40:09 +0300

    📝 docs(WatchListViewController.swift): add comments to describe properties and methods This commit adds comments to the `WatchListViewController.swift` file to describe the purpose of each property and method. The comments provide a clear understanding of the functionality of the view controller and its components. The comments also include information on the built-in methods of the `UITableViewDelegate` and `UITableViewDataSource` protocols in iOS.

--- Stocks/Controllers/WatchListViewController.swift
+++ Stocks/Controllers/WatchListViewController.swift
@@ -13,8 +13,12 @@ import UIKit
 class WatchListViewController: UIViewController {
   // MARK: - Internal
 
+  /// This property is a static variable of type `CGFloat` that is used to determine the maximum width of a label that displays the change percentage of a stock.
+  /// This property is used to help format the label so that it aligns properly with other labels in the table view cell.
   static var maxChangeWidth: CGFloat = 0
 
+  /// This method is an overridden method in a view controller that is called when the view of the view controller is loaded into memory.
+  /// This method is responsible for setting up the initial state of the view controller, including adding subviews, setting up data sources, and fetching data.
   override func viewDidLoad() {
     super.viewDidLoad()
 
@@ -28,6 +32,8 @@ class WatchListViewController: UIViewController {
     setUpObserver()
   }
 
+  /// This method is an overridden method in a view controller that is called every time the view of the view controller is laid out.
+  /// This method is responsible for adjusting the frame of a `UITableView` to make sure it takes up the full bounds of the view controller's view.
   override func viewDidLayoutSubviews() {
     super.viewDidLayoutSubviews()
     tableView.frame = view.bounds
@@ -35,16 +41,26 @@ class WatchListViewController: UIViewController {
 
   // MARK: - Private
 
+  /// This property is a private optional `Timer` variable that is used to control the delay before initiating a search operation.
+  /// If the user continues to type before the timer is finished, it will invalidate the current timer and start a new one.
   private var searchTimer: Timer?
 
+  /// This property is a private optional `FloatingPanelController` variable that is used to manage the floating panel that is displayed on the view controller.
   private var floatingPanelController: FloatingPanelController?
 
+  /// This property is a private dictionary variable that is used to store an array of `CandleStick` objects associated with each stock symbol. The keys of the dictionary are the stock symbols.
   private var watchlistMap: [String: [CandleStick]] = [:]
 
+  /// This property is a private array variable that is used to store an array of `WatchListTableViewCell.ViewModel` objects.
+  /// These view models are used to populate the cells of the table view displayed on the view controller.
   private var viewModels: [WatchListTableViewCell.ViewModel] = []
 
+  /// This property is a private optional `NSObjectProtocol` variable that is used to observe changes in watchlist data.
+  /// When the observer is set up, it will call a specified method in the view controller whenever the watchlist data changes.
   private var observer: NSObjectProtocol?
 
+  /// This property is a private constant `UITableView` variable that is used to display a table view on the view controller.
+  /// The table view is set up with a registered `WatchListTableViewCell` class and identifier.
   private let tableView: UITableView = {
     let table = UITableView()
     table.register(
@@ -54,6 +70,9 @@ class WatchListViewController: UIViewController {
     return table
   }()
 
+  /// This method is a private method that is used to set up a `UITableView` within a view controller.
+  /// This method is responsible for adding the `UITableView` as a subview of the view controller's view
+  /// and setting its delegate and data source to the view controller itself.
   private func setUpTableView() {
     view.addSubview(tableView)
     tableView.delegate = self
@@ -298,10 +317,26 @@ extension WatchListViewController: FloatingPanelControllerDelegate {
 // MARK: UITableViewDelegate, UITableViewDataSource
 
 extension WatchListViewController: UITableViewDelegate, UITableViewDataSource {
+  /// This method is a built-in method of the `UITableViewDataSource` protocol in iOS.
+  /// This method is called by a table view object to determine the number of rows to
+  /// be displayed in a particular section of the table view.
+  ///
+  /// - Parameters:
+  ///   - tableView: The table view requesting this information.
+  ///   - section: An index number identifying a section of the table view.
+  /// - Returns: An integer value indicating the number of rows in the specified section.
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return viewModels.count
   }
 
+  /// This method is a built-in method of the `UITableViewDataSource` protocol in iOS.
+  /// It is called by the table view object to retrieve the cell for a particular row
+  /// in a given section of the table view.
+  ///
+  /// - Parameters:
+  ///   - tableView: The table view requesting this information.
+  ///   - indexPath: An index path locating a row in tableView.
+  /// - Returns: A UITableViewCell object representing the row at the specified index path.
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     guard let cell = tableView.dequeueReusableCell(
       withIdentifier: WatchListTableViewCell.identifier,
@@ -312,10 +347,23 @@ extension WatchListViewController: UITableViewDelegate, UITableViewDataSource {
     return cell
   }
 
+  /// This method is a built-in method of the `UITableViewDelegate` protocol in iOS.
+  /// This method is called by the table view object to determine the height of a row
+  /// at a specific index path.
+  ///
+  /// - Parameters:
+  ///   - tableView: The table view requesting this information.
+  ///   - indexPath: An index path locating a row in tableView.
+  /// - Returns: A `CGFloat` value representing the height of the row at the specified index path.
   func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
     return WatchListTableViewCell.prefferedHeight
   }
 
+  /// This method is a delegate method from the `UITableViewDelegate` protocol that gets called whenever a user selects a row in a `UITableView`. This method is used to provide custom behavior when a row is selected.
+  ///
+  /// - Parameters:
+  ///   - tableView: The UITableView instance that the user interacted with.
+  ///   - indexPath: The IndexPath of the selected row.
   func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     tableView.deselectRow(at: indexPath, animated: true)
 
@@ -329,10 +377,21 @@ extension WatchListViewController: UITableViewDelegate, UITableViewDataSource {
     present(navigationController, animated: true)
   }
 
+  /// This is a method of the `UITableViewDelegate` protocol that determines whether a row at a specified index path can be edited.
+  ///
+  /// - Parameters:
+  ///   - tableView: The table view requesting this information.
+  ///   - indexPath: The index path of the row being edited.
+  /// - Returns: A Boolean value indicating whether the row specified at the index path can be edited.When the value is true, the row can be edited. When the value is false, the row cannot be edited.
   func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
     return true
   }
 
+  /// This is a method of the `UITableViewDelegate` protocol that specifies the editing style for a row at a specified index path.
+  ///
+  /// - Parameters:
+  ///   - tableView: The table view requesting this information.
+  ///   - indexPath: The index path of the row for which to return the editing style.
   func tableView(
     _ tableView: UITableView,
     editingStyleForRowAt indexPath: IndexPath
@@ -341,6 +400,12 @@ extension WatchListViewController: UITableViewDelegate, UITableViewDataSource {
     return .delete
   }
 
+  /// This is a method of the `UITableViewDelegate` protocol that handles row deletion and other editing operations.
+  ///
+  /// - Parameters:
+  ///   - tableView: The table view requesting this information.
+  ///   - editingStyle: The editing style associated with the row at the specified index path.
+  ///   - indexPath: The index path of the row being edited.
   func tableView(
     _ tableView: UITableView,
     commit editingStyle: UITableViewCell.EditingStyle,
@@ -363,6 +428,8 @@ extension WatchListViewController: UITableViewDelegate, UITableViewDataSource {
 // MARK: WatchListTableViewCellDelegate
 
 extension WatchListViewController: WatchListTableViewCellDelegate {
+  /// This method is a function that is called when the maximum width of a label that displays the change percentage of a stock in a table view cell has been updated.
+  /// This method is responsible for reloading the table view to apply the updated formatting of the label.
   func didUpdateMaxWidth() {
     // MARK: - TODO: Only refresh rows prior to the current row that changes to max width
 
